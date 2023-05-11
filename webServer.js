06726bc91312ffb7b6eab91177be101b1a9f7e12
IT20651060 <it20651060@my.sliit.lk>
2023-05-11 16:17:43 +0530

    commets

--- webServer.js
+++ webServer.js
@@ -14,3 +14,14 @@ http
     res.end();
   })
   .listen(8090);
+
+//   The first line loads the http module into the http variable, making its functions and properties available for use in the code.
+// The second line uses the http.createServer() function to create a new HTTP server that listens for incoming requests.
+// The createServer() function takes a callback function as an argument, which will be executed every time a request is received by the server. This callback function takes two parameters: req (the incoming request object) and res (the response object that will be sent back to the client).
+// The res.writeHead() function sets the response header to indicate that the response will be HTML. The 200 argument is the HTTP status code, which indicates that the request was successful.
+// The res.write() function writes the response body, which in this case is the string "Hello World!".
+// The res.end() function ends the response and sends it back to the client.
+// The listen() function starts the server and specifies the port number (in this case, port 8080) on which the server should listen for incoming requests.
+// In order to use this code in your Node.js project, you can add it to a new or existing JavaScript file in your project, such as index.js or app.js.
+
+// To run the web server, you can run the JavaScript file in your terminal using the node command:
